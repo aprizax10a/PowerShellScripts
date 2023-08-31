6cc127e744bad2a77cb0d6fa5b2d59b5daf839ee
aprizax10a <75101006+aprizax10a@users.noreply.github.com>
2023-08-30 20:02:45 -0400

    Add files via upload

---
+++ Exchange-RemoveDelegatedPermission.ps1
@@ -0,0 +1,23 @@
+﻿Import-Module ActiveDirectory
+Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
+
+$DatabaseName = "KADIV"
+$DelegatedUser = "IEB\asaba"
+$AllMailbox = Get-MailboxDatabase -Identity $DatabaseName | Get-Mailbox
+
+foreach($Mailbox in $AllMailbox)
+{
+    $MailboxUPN = $Mailbox.UserPrincipalName
+    $CurrentPermission = Get-MailboxPermission -Identity $MailboxUPN -User $DelegatedUser
+    $AsabaPermission = $CurrentPermission | Where-Object {$_.IsInherited -eq $False -and $_.Deny -eq $False}
+    If([string]::IsNullOrEmpty($AsabaPermission))
+    {
+        Write-Host "$($DelegatedUser) has no permission on $($MailboxUPN) mailbox" -ForegroundColor Yellow
+    }
+    Else
+    {
+        Write-Host "$($DelegatedUser) has permission on $($MailboxUPN) mailbox" -ForegroundColor Green
+        Write-Host "Removing $($DelegatedUser) permission on $($MailboxUPN) mailbox" -ForegroundColor Green
+        Remove-MailboxPermission -Identity $MailboxUPN -User $DelegatedUser -AccessRights FullAccess
+    }
+}
