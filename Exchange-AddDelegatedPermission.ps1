e22c8c2baac032fa6180c761e2098c8b2e350d18
aprizax10a <75101006+aprizax10a@users.noreply.github.com>
2023-08-30 20:03:28 -0400

    Add files via upload

---
+++ Exchange-AddDelegatedPermission.ps1
@@ -0,0 +1,23 @@
+﻿Import-Module ActiveDirectory
+Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
+
+$DatabaseName = "KADIV"
+$DelegatedUser = "IEB\asaba"
+$AllMailbox = Get-MailboxDatabase -Identity $DatabaseName | Get-Mailbox
+
+foreach($Mailbox in $AllMailbox)
+{
+    $MailboxUPN = $Mailbox.UserPrincipalName
+    $CurrentPermission = Get-MailboxPermission -Identity $MailboxUPN -User $DelegatedUser
+    $AsabaPermission = $CurrentPermission | Where-Object {$_.IsInherited -eq $False -and $_.Deny -eq $False}
+    If([string]::IsNullOrEmpty($AsabaPermission))
+    {
+        Write-Host "$($DelegatedUser) has no permission on $($MailboxUPN) mailbox" -ForegroundColor Yellow
+        Write-Host "Added $($DelegatedUser) permission on $($MailboxUPN) mailbox" -ForegroundColor Yellow
+        Add-MailboxPermission -Identity $MailboxUPN -AccessRights FullAccess -User $DelegatedUser
+    }
+    Else
+    {
+        Write-Host "$($DelegatedUser) already has permission on $($MailboxUPN) mailbox" -ForegroundColor Green
+    }
+}
